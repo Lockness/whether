2fa595cdc96c42f41399f76f3cd18b8f3e59326c
Justin Carruthers <jtcruthers@gmail.com>
2019-01-20 04:24:43 -0500

    Disabled time in the past

--- src/pages/HomePage/MainSearch/DatePicker.js
+++ src/pages/HomePage/MainSearch/DatePicker.js
@@ -5,12 +5,38 @@ import PropTypes from 'prop-types';
 
 import { Form, DatePicker } from 'antd';
 
+import moment from 'moment';
+
 class DateOfTripPicker extends React.Component {
+
+  getArray = (start, end) => {
+    const result = [];
+    for (let i = start; i < end; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+
+  getDisabledDate = (current) => {
+    return current && current < moment().subtract(1, 'days');
+  }
+
+  getDisabledTime = () => {
+    let hours = moment().hours()
+    return {
+      disabledHours: () => this.getArray(0,24).splice(0, hours)
+    }
+  }
 
   render() {
     return (
       <Form.Item label="Date of Trip" style={{'textAlign': 'left'}}>
-        <DatePicker pullLeft
+        <DatePicker
+          pullLeft
+          showTime
+          format='MM-DD-YYYY hh:mm'
+          disabledDate={this.getDisabledDate}
+          disabledTime={this.getDisabledTime}
           onChange={(date, dateString) => this.props.onChange(dateString)}
         />
       </Form.Item>
