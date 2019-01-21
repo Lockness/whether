b50ee74dd49c3422c39b5aa3d7609e4b3a83e355
Justin Carruthers <jtcruthers@gmail.com>
2019-01-20 23:56:04 -0500

    Hitting API on search

--- src/pages/HomePage/MainSearch/MainSearch.js
+++ src/pages/HomePage/MainSearch/MainSearch.js
@@ -7,7 +7,10 @@ import PropTypes from 'prop-types';
 
 import { Form, Button } from 'antd';
 
+import axios from 'axios';
+
 class MainSearch extends React.Component {
+
   state = {
     origin: '',
     destination: '',
@@ -20,6 +23,20 @@ class MainSearch extends React.Component {
 
   onDateChange = (date) => {
     this.setState({'date': date});
+  }
+
+  onSearch = () => {
+    let url = 'https://osjmxav54i.execute-api.us-east-1.amazonaws.com/beta/directions?origin=disneyland&destination=home'
+    axios.get(
+      url,
+      {
+        params: {
+          headers: {'Access-Control-Allow-Origin': '*'}
+        }
+      }
+    ).then(res => {
+      console.log(res)
+    });
   }
 
   render() {
@@ -35,7 +52,11 @@ class MainSearch extends React.Component {
           onChange={this.onDateChange}
         />
         <Form.Item>
-          <Button>Search</Button>
+          <Button
+            onClick={this.onSearch}
+          >
+            Search
+          </Button>
         </Form.Item>
       </Form>
     );
