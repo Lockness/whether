e129d0d1286cbd531a54f6b345e643ffe91eb52c
Justin Carruthers <jtcruthers@gmail.com>
2019-07-09 18:07:22 -0400

    Separated logic into multiple components for results

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -1,56 +1,61 @@
-'use-strict';
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Map from './Map';
 // import WithMockApiData from './WithMockApiData';
 import WeatherPoints from './WeatherPoints';
-import Spinner from '../../components/Spinner/';
+import Spinner from '../../components/Spinner';
 
-const ResultsPage = ({origin, destination, waypoints, polyline}) => {
+const LoadingPage = () => (
+  <React.Fragment>
+    <p className="align-middle text-center text-3xl mt-32">
+      Loading your trip{' '}
+      <span role="img" aria-label="smile">
+        😁
+      </span>
+    </p>
+    <Spinner />
+  </React.Fragment>
+);
 
-  let outputJsx = (
-    <React.Fragment>
-      <p className="align-middle text-center text-3xl mt-32">
-        Loading your trip{' '}
-        <span role="img" aria-label="smile">
-          😁
-        </span>
-      </p>
-      <Spinner />
-    </React.Fragment>
-  );
-  if (waypoints && waypoints.length > 0) {
-    outputJsx = (
-      <div id="results" className="text-center h-full">
-        <div className="mt-1 mb-1 p-1">
-          <h1 className="mb-1">Heres the weather for the trip</h1>
-          <p>
-            Starting in {origin} and finishing in {destination}
-          </p>
+const ResultsPage = ({ origin, destination, waypoints, polyline }) => {
+  if (!waypoints || !waypoints.length > 0) {
+    return <LoadingPage />;
+  }
+  return (
+    <div id="results" className="text-center h-full">
+      <div className="mt-1 mb-1 p-1">
+        <h1 className="mb-1">Heres the weather for the trip</h1>
+        <p>
+          Starting in {origin} and finishing in {destination}
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row h-full">
+        <div className="w-full h-full">
+          <Map waypoints={waypoints} polyline={polyline} />
         </div>
-        <div className="flex flex-col md:flex-row h-full">
-          <div className="w-full h-full">
-            <Map waypoints={waypoints} polyline={polyline} />
-          </div>
-          <div className="h-full overflow-scroll w-full md:w-half">
-            <WeatherPoints waypoints={waypoints} />
-          </div>
+        <div className="h-full overflow-scroll w-full md:w-half">
+          <WeatherPoints waypoints={waypoints} />
         </div>
       </div>
-    );
-  }
-
-  return outputJsx;
+    </div>
+  );
 };
 
-const mapStateToProps = (state, props) => ({
+ResultsPage.propTypes = {
+  origin: PropTypes.string,
+  destination: PropTypes.string,
+  waypoints: PropTypes.arrayOf(PropTypes.object),
+  polyline: PropTypes.string
+};
+
+const mapStateToProps = state => ({
   origin: state.places.origin,
   destination: state.places.destination,
   waypoints: state.whether.waypoints,
   polyline: state.whether.polyline
-})
+});
 
 export default connect(mapStateToProps)(ResultsPage);
 // export default WithMockApiData(ResultsPage);
--- src/pages/ResultsPage/WeatherPoints.js
+++ src/pages/ResultsPage/WeatherPoints.js
@@ -3,35 +3,59 @@ import PropTypes from 'prop-types';
 
 import WeatherIcon from '../../components/WeatherIcon';
 
-const WeatherPoints = ({ waypoints }) => {
-  const getAddressCity = address => {
-    const cityLocation = 1;
-    const stateLocation = 2;
-    const delimiter = ',';
-    const split = address.split(delimiter);
-    const city = split[cityLocation];
-    const state = split[stateLocation].trim().split(/[ ]+/)[0];
-    return city + ', ' + state;
-  };
+const getAddressCity = address => {
+  const cityLocation = 1;
+  const stateLocation = 2;
+  const delimiter = ',';
+  const split = address.split(delimiter);
+  const city = split[cityLocation];
+  const state = split[stateLocation].trim().split(/[ ]+/)[0];
+  return `${city}, ${state}`;
+};
 
+const getArrivalTime = () => {
+  return 'Time';
+};
+
+const WeatherCard = ({ arrivalTime, city, forecast, position }) => {
+  return (
+    <div className="w-full bg-white text-indigo h-32 shadow border-2 border-indigo-light rounded">
+      <h4 className="mb-2 mt-1 py-1 border-b-2 border-grey">{`${position} : ${city}`}</h4>
+      <p className="text-center align-middle">{forecast}</p>
+      <div>{arrivalTime}</div>
+      <div className="mt-3">
+        <WeatherIcon forecast={forecast} />
+      </div>
+    </div>
+  );
+};
+WeatherCard.propTypes = {
+  arrivalTime: PropTypes.string,
+  city: PropTypes.string,
+  forecast: PropTypes.string,
+  position: PropTypes.number
+};
+
+const WeatherPoints = ({ waypoints }) => {
+  const startTime = new Date();
   return (
     <div className="flex flex-col justify-center">
       {waypoints.map((waypoint, index) => {
-        let city = getAddressCity(waypoint.address);
+        const arrivalTime = getArrivalTime(startTime, waypoint.arrivalTime);
+        const city = getAddressCity(waypoint.address);
         return (
-          <div key={index} className="w-full bg-white text-indigo h-32 shadow border-2 border-indigo-light rounded">
-            <h4 className="mb-2 mt-1 py-1 border-b-2 border-grey">{index + 1 + ':' + city}</h4>
-            <p className="text-center align-middle">{waypoint.weather_data.shortForecast}</p>
-            <div className="mt-3">
-              <WeatherIcon forecast={waypoint.weather_data.shortForecast} />
-            </div>
-          </div>
+          <WeatherCard
+            key={index}
+            arrivalTime={arrivalTime}
+            city={city}
+            forecast={waypoint.weather_data.shortForecast}
+            position={index + 1}
+          />
         );
       })}
     </div>
   );
 };
-
 WeatherPoints.propTypes = {
   waypoints: PropTypes.arrayOf(
     PropTypes.shape({
